64e897ada41f75b81a48edf387aa7f739e08f661
dlyt <miao22722@163.com>
2016-11-30 19:29:12 +0800

    修改时间格式

--- routes/business/share/index.js
+++ routes/business/share/index.js
@@ -2,6 +2,7 @@
 
 const lightco = require('lightco')
 const express = require('express')
+const moment = require('moment')
 const logger = log4js.getLogger('[routes-business-share]')
 const router = express.Router()
 
@@ -43,6 +44,9 @@ function share(req, res) {
 
         var [err, share] = yield BigMatchSerieShare.scope('detail').findOne(opts)
         if (err) throw err
+
+        share.bigMatchSerie.start_date = moment(share.bigMatchSerie.start_date).format('YYYY年MM月D日')
+        share.bigMatchSerie.end_date = moment(share.bigMatchSerie.end_date).format('YYYY年MM月D日')
 
         res.json(Conf.promise('0', share))
 
